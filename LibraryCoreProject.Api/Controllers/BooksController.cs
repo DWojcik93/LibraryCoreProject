6c7e5b5427349e196c17268cdb810489ffd1f24d
Damian Wójcik <mamian@autograf.pl>
2020-10-29 18:30:27 +0100

    Add Api

--- LibraryCoreProject.Api/Controllers/BooksController.cs
+++ LibraryCoreProject.Api/Controllers/BooksController.cs
@@ -1,29 +1,30 @@
 ﻿using LibraryCoreProject.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http;
 
-namespace LibraryCoreProject.WebApp.Controllers
+namespace LibraryCoreProject.Api.Controllers
 {
-    [RoutePrefix("api/[controller]")]
-    public class LibraryApiController : ApiController
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
     {
         private readonly IBookManager _manager;
-        private readonly ILogger<LibraryApiController> _logger;
+        private readonly ILogger<BooksController> _logger;
 
-        public LibraryApiController(IBookManager manager,
-                                    ILogger<LibraryApiController> logger)
+        public BooksController(IBookManager manager,
+                                    ILogger<BooksController> logger)
         {
             _manager = manager;
             _logger = logger;
         }
 
         [HttpGet]
-        public async Task<IHttpActionResult> Get()
+        public async Task<IActionResult> Get()
         {
             try
             {
@@ -40,7 +41,7 @@ namespace LibraryCoreProject.WebApp.Controllers
         }
 
         [HttpGet, Route("{guid:guid}")]
-        public async Task<IHttpActionResult> Get(string guid)
+        public async Task<IActionResult> Get(string guid)
         {
             try
             {
